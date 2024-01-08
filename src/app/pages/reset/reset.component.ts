84a6bf05a12a2c3648be3bedc439efa6f8e94564
Marco Torres <marcoantonio.torres@icloud.com>
2024-01-08 16:57:31 -0500

    This is part 2 of 2. In this version I add functionality to send the token along with the reset password request. To do this in we refractor the code that is in the submit method in reset.component.ts. We remove getRawValue reset method and instead create a const property and assing the data to that property. Next I create a new const propery called data, in here I create a object and spread the property formData so I can add a new property called token. Here we use ActivatedRoute (This is a service provided by the Angular Router module. It represents the route that is currently active and contains information about the route, its parameters, data, and the route's associated component. The  service allows us to access information about the currently active route in our angular applcaiton). Furthermore, through the use of snapshop (This provides a snapshot of the route information at the moment the component was created. We can access parameters, data, and other information directly from the snapshot) and params (This property is an observable that allows us to subscribe to changes in the route parameter. This is useful when dealing with dynamic route paramters) we pass the token (the token string matches the string in the app-routing.module.ts file located in Routes). Once this data is collected we pass the data along to the forgotService reset method and the observer navigates us back to the login page. Lastly, in the forgot.service.ts file I add the reset method, which takes in the body as a parameter.

--- src/app/pages/reset/reset.component.ts
+++ src/app/pages/reset/reset.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ForgotService } from 'src/app/services/forgot.service';
 
 @Component({
@@ -8,12 +9,29 @@ import { ForgotService } from 'src/app/services/forgot.service';
   styleUrls: ['./reset.component.scss']
 })
 export class ResetComponent implements OnInit{
+  // ActivatedRoute is a service provided by the Angular Router module. It represents the route that 
+  // is currently active and contains information about the route, its parameters, data, and the route's
+  // associated component. The `ActivateRoute` service allows us to access information about the currently active 
+  // route in our angular applciaton
+
+  // snapshot - provides a snapshot of the route information at the moment the component was created. We can access
+  // parameters, data, and other infromation directly from the snapshot
+
+  // params - this property in an observable that allows us to subscribe to changes in the route parameter.
+  // This is useful when dealing with dynamic route parameters
+
+  // data - This property provides access to the static data associated with the route. We can define data in 
+  // in the route configuration. 
 
   // Bang opperator to initialize later
   form!: FormGroup; 
   
   constructor(private formBuilder: FormBuilder, 
-              private forgotService: ForgotService){}
+              private forgotService: ForgotService,
+              // route used to get token from the URL
+              private route: ActivatedRoute,
+              private router: Router
+              ){}
   
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -23,8 +41,19 @@ export class ResetComponent implements OnInit{
   }
   
   submit(){
-    this.forgotService.forgot(this.form.getRawValue()).subscribe( () => {
-      
+    const formData = this.form.getRawValue()
+    const data = {
+      // Below I have password and password confirm
+      // I spread the properties of formData and 
+      ...formData,
+      // add the new property token
+      token: this.route.snapshot.params['token']  // matches name to the token in Routes 
+    }
+
+
+    this.forgotService.reset(data).subscribe( () => {
+      // after the data is sent we want to re-direct
+      this.router.navigate(['/login'])
     })
   }
 
